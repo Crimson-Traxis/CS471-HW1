e2a7c78d728250b15ac97064e802de8f78241093
Trent Killinger <trentkillinger@gmail.com>
2018-01-29 06:19:15 -0500

    Initial Push

---
+++ CS471-HW1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS471_HW1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Modles.CartItem> CartItems = new ObservableCollection<Modles.CartItem>();
+
+        public MainWindow()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private async void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            await DialogHost.Show("", "RootDialog");
+        }
+    }
+}
---
+++ CS471-HW1/Modles/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS471_HW1.Modles
+{
+    public class CartItem
+    {
+        public string Name { get; set; }
+        public string ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
