bdcde8b7baead667dbc7ed354fbeb562e5d6e112
Trent Killinger <trentkillinger@gmail.com>
2018-02-01 22:07:12 -0500

    Added Store Screen

--- CS471-HW1/MainWindow.xaml.cs
+++ CS471-HW1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,17 +25,42 @@ namespace CS471_HW1
     public partial class MainWindow : Window
     {
         public ObservableCollection<Modles.CartItem> CartItems = new ObservableCollection<Modles.CartItem>();
+        public List<string> MenuItems = new List<string> { "Store", "Cart", "Summary" };
 
         public MainWindow()
         {
             this.DataContext = this;
             InitializeComponent();
+
+            CartItemsListBox.DataContext = CartItems;
+            MenuListBox.DataContext = MenuItems;
+
+            LoadScreen("Store");
+
+            CartItems.CollectionChanged += CartItems_CollectionChanged;
+
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text", Quantity = 7 });
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text1", Quantity = 6 });
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text2", Quantity = 5 });
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text3", Quantity = 4 });
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text4", Quantity = 3 });
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text5", Quantity = 2 });
+            CartItems.Add(new Modles.CartItem { ItemId = "", Name = "Text6", Quantity = 1 });
+        }
+
+        private void CartItems_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            CountingBadge.Badge = CartItems.Count;
         }
 
         private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             //until we had a StaysOpen glag to Drawer, this will help with scroll bars
             var dependencyObject = Mouse.Captured as DependencyObject;
+            //dependencyObject.GetValue()
+
+            LoadScreen(((ListBox)sender).SelectedValue.ToString());
+
             while (dependencyObject != null)
             {
                 if (dependencyObject is ScrollBar) return;
@@ -42,6 +68,24 @@ namespace CS471_HW1
             }
 
             MenuToggleButton.IsChecked = false;
+        }
+
+        private void LoadScreen(string screen)
+        {
+            switch(screen)
+            {
+                case "Store":
+                    MainContent.Children.Clear();
+                    MainContent.Children.Add(new UserControls.Store());
+
+                    break;
+                case "Cart":
+
+                    break;
+                case "Summary":
+
+                    break;
+            }
         }
 
         private async void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
